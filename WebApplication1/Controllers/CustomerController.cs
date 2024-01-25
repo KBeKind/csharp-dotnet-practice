81de8660a6303269dfe023d016df6bf2ce4db167
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-01-24 18:04:51 -0600

    commit

---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public static List<Customer> customers = new List<Customer>()
+        {
+            new Customer() { Id = 1, Name="Duder", Amount= 12000}
+            , new Customer() { Id = 2, Name="Dudette", Amount= 12000}
+        };
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Customer Management System";
+            ViewBag.CustomerCount = customers.Count();
+            ViewBag.Customers = customers;
+            return View();
+        }
+
+
+        public IActionResult Details()
+        {
+
+            ViewData["Message"] = "Customer Details Message";
+            ViewData["CustomerCount"] = customers.Count();
+            ViewData["CustomerList"] = customers;
+
+            ViewBag.Customers = customers;
+
+
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Method()
+        {
+            TempData["Message"] = "Method 1 Message";
+            //TempData["CustomerCount"] = customers.Count();
+            //TempData["CustomerList"] = customers;
+
+
+            return View();
+        }
+
+
+        public IActionResult MethodTwo()
+        {
+
+            if (TempData["Message"] == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["Message"] = TempData["Message"];
+             
+                //TempData.Clear(); // clear the tempdata after use. 
+                //                  // this is to prevent the same data from being used again. 
+             
+                return View();
+            }
+          
+        }
+
+
+    }
+}
---
+++ WebApplication1/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Customer
+    {
+
+        public int Id {get; set;}
+
+        public String? Name { get; set;}
+        
+        public double Amount { get; set;}
+
+    }
+}
